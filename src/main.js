6c839e3c0bbbf3c27742288d307bb34344f70a91
yang young cheol <202106002@gbsks.ac.kr>
2022-04-11 08:10:15 +0900

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import mitt from "mitt"
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 let emitter = mitt();
 let app = createApp(App);
 app.config.globalProperties.emitter = emitter;
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 import store from './Store';
 import './registerServiceWorker'
